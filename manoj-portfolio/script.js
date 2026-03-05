9d7290bb1cea5b08e6e7a10369d1408cb89d7509
manoj5412 <manojmanoj4624@gmail.com>
2026-03-05 14:48:40 +0530

    Add files via upload

---
+++ manoj-portfolio/script.js
@@ -0,0 +1,9 @@
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 80 },
+    size: { value: 3 },
+    move: { speed: 1 },
+    line_linked: { enable: true },
+    color: { value: "#38bdf8" }
+  }
+});
